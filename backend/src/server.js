d1f6de2c6a354f66758ed5f697d3ff29f26481c6
DabralPriyanshu <priyanshudabral07@gmail.com>
2025-12-09 19:41:53 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 dotenv.config({ quite: true });
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 const app = express();
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "API is live" });
-});
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
