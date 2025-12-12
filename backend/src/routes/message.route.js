5a502661ad43443291ad1dd4606cc8500492401e
DabralPriyanshu <priyanshudabral07@gmail.com>
2025-12-12 12:58:52 +0530

    message routes and controllers implemented

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,17 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMessagesByUserId,
+  sendMessage,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectedRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
-router.post("/signup", (req, res) => {
-  res.send("signup");
-});
+router.use(arcjetProtection, protectedRoute);
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
